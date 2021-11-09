a81147e9045b6a2081bee3a1eef08e62f2f86052
Ankur <ankurprasad910@gmail.com>
2021-11-09 20:51:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let screen = document.querySelector('#screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
+
+
+for(item of buttons){
+    item.addEventListener('click', (e)=> {
+        let buttonText = e.target.innerText;
+        
+        if(buttonText == 'x'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } 
+        
+        else if(buttonText == "c"){
+            screenValue = '';
+            screen.value = screenValue
+        } 
+        
+        else if(buttonText == "=") {
+            screen.value = eval(screenValue)
+        } 
+
+        else if(buttonText == "÷") {
+            buttonText = '/';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
